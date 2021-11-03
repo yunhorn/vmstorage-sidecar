678c743b66fbdc7ee8581cbe8f63c2de02162798
Your Name <hello@exmaple.com>
2021-11-03 10:20:36 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,33 @@
+module vmbackup
+
+go 1.17
+
+require (
+	github.com/VictoriaMetrics/VictoriaMetrics v1.68.0
+	github.com/aws/aws-sdk-go v1.41.13
+)
+
+require (
+	cloud.google.com/go v0.97.0 // indirect
+	cloud.google.com/go/storage v1.18.2 // indirect
+	github.com/VictoriaMetrics/metrics v1.18.0 // indirect
+	github.com/VictoriaMetrics/metricsql v0.27.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/valyala/fastrand v1.1.0 // indirect
+	github.com/valyala/histogram v1.2.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
+	golang.org/x/sys v0.0.0-20211020174200-9d6173849985 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.59.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20211021150943-2b146023228c // indirect
+	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/VictoriaMetrics/VictoriaMetrics/app/vmbackup/snapshot"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/backup/actions"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/backup/common"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/backup/fslocal"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/backup/fsnil"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/backup/s3remote"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/buildinfo"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/envflag"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/flagutil"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	storageDataPath   = flag.String("storageDataPath", "victoria-metrics-data", "Path to VictoriaMetrics data. Must match -storageDataPath from VictoriaMetrics or vmstorage")
+	snapshotName      = flag.String("snapshotName", "", "Name for the snapshot to backup. See https://docs.victoriametrics.com/Single-server-VictoriaMetrics.html#how-to-work-with-snapshots. There is no need in setting -snapshotName if -snapshot.createURL is set")
+	snapshotCreateURL = flag.String("snapshot.createURL", "", "VictoriaMetrics create snapshot url. When this is given a snapshot will automatically be created during backup. "+
+		"Example: http://victoriametrics:8428/snapshot/create . There is no need in setting -snapshotName if -snapshot.createURL is set")
+	snapshotDeleteURL = flag.String("snapshot.deleteURL", "", "VictoriaMetrics delete snapshot url. Optional. Will be generated from -snapshot.createURL if not provided. "+
+		"All created snapshots will be automatically deleted. Example: http://victoriametrics:8428/snapshot/delete")
+	dst = flag.String("dst", "", "Where to put the backup on the remote storage. "+
+		"Example: gs://bucket/path/to/backup/dir, s3://bucket/path/to/backup/dir or fs:///path/to/local/backup/dir\n"+
+		"-dst can point to the previous backup. In this case incremental backup is performed, i.e. only changed data is uploaded")
+	origin            = flag.String("origin", "", "Optional origin directory on the remote storage with old backup for server-side copying when performing full backup. This speeds up full backups")
+	concurrency       = flag.Int("concurrency", 10, "The number of concurrent workers. Higher concurrency may reduce backup duration")
+	maxBytesPerSecond = flagutil.NewBytes("maxBytesPerSecond", 0, "The maximum upload speed. There is no limit if it is set to 0")
+)
+
+var (
+	access_key string
+	secret_key string
+	end_point  string
+	bucket     string
+	sess       *session.Session
+	svc        *s3.S3
+)
+
+func init() {
+	access_key = os.Getenv("AWS_ACCESS_KEY_ID")
+	secret_key = os.Getenv("AWS_SECRET_ACCESS_KEY")
+}
+
+func clean() {
+	buckResp, _ := svc.ListBuckets(&s3.ListBucketsInput{})
+	for _, bt := range buckResp.Buckets {
+		fmt.Println(*bt.Name)
+		params := &s3.ListObjectsInput{
+			Bucket: aws.String(*bt.Name),
+		}
+		resp, err := svc.ListObjects(params)
+		if err != nil {
+			panic(err)
+		}
+		for _, item := range resp.Contents {
+			fmt.Println("Name:         ", *item.Key)
+			fmt.Println("Last modified:", *item.LastModified)
+			fmt.Println("Size:         ", *item.Size)
+			fmt.Println("Storage class:", *item.StorageClass)
+			fmt.Println("")
+		}
+
+	}
+	log.Println("bucket.size:", len(buckResp.Buckets))
+}
+
+func main() {
+
+	// Write flags and help message to stdout, since it is easier to grep or pipe.
+	flag.CommandLine.SetOutput(os.Stdout)
+	flag.Usage = usage
+	envflag.Parse()
+	buildinfo.Init()
+	logger.Init()
+
+	if len(*snapshotCreateURL) > 0 {
+		if len(*snapshotName) > 0 {
+			logger.Fatalf("-snapshotName shouldn't be set if -snapshot.createURL is set, since snapshots are created automatically in this case")
+		}
+		logger.Infof("Snapshot create url %s", *snapshotCreateURL)
+		if len(*snapshotDeleteURL) <= 0 {
+			err := flag.Set("snapshot.deleteURL", strings.Replace(*snapshotCreateURL, "/create", "/delete", 1))
+			if err != nil {
+				logger.Fatalf("Failed to set snapshot.deleteURL flag: %v", err)
+			}
+		}
+		logger.Infof("Snapshot delete url %s", *snapshotDeleteURL)
+
+		name, err := snapshot.Create(*snapshotCreateURL)
+		if err != nil {
+			logger.Fatalf("cannot create snapshot: %s", err)
+		}
+		err = flag.Set("snapshotName", name)
+		if err != nil {
+			logger.Fatalf("cannot set snapshotName flag: %v", err)
+		}
+
+		defer func() {
+			err := snapshot.Delete(*snapshotDeleteURL, name)
+			if err != nil {
+				logger.Fatalf("cannot delete snapshot: %s", err)
+			}
+		}()
+	}
+
+	dstFS, err := newDstFS(bucket)
+	if err != nil {
+		logger.Fatalf("%s", err)
+	}
+	s := dstFS.(*s3remote.FS)
+	log.Println("===================", s.CustomEndpoint, s.Bucket)
+	end_point = s.CustomEndpoint
+	bucket = s.Bucket
+
+	sess, err := session.NewSession(&aws.Config{
+		Credentials:      credentials.NewStaticCredentials(access_key, secret_key, ""),
+		Endpoint:         aws.String(end_point),
+		Region:           aws.String("hongkong"),
+		DisableSSL:       aws.Bool(true),
+		S3ForcePathStyle: aws.Bool(false), //virtual-host style方式，不要修改
+	})
+	if err != nil {
+		panic(err)
+	}
+	svc = s3.New(sess)
+
+	log.Println("end_point:", end_point)
+	clean()
+
+	c := time.Tick(5 * time.Second)
+	go func() {
+		for {
+			<-c
+			go clean()
+		}
+	}()
+
+	beginBackup()
+	select {}
+}
+
+func beginBackup() {
+	dayhour := time.Now().Format("2006-01-02-15-04")
+	bucket := *dst + "/" + dayhour
+	srcFS, err := newSrcFS()
+	if err != nil {
+		logger.Fatalf("%s", err)
+	}
+	originFS, err := newOriginFS()
+	if err != nil {
+		logger.Fatalf("%s", err)
+	}
+
+	// newFs(*dst)
+	dstFS, err := newDstFS(bucket)
+	if err != nil {
+		logger.Fatalf("%s", err)
+	}
+	//TODO init s3 client
+	a := &actions.Backup{
+		Concurrency: *concurrency,
+		Src:         srcFS,
+		Dst:         dstFS,
+		Origin:      originFS,
+	}
+	if err := a.Run(); err != nil {
+		logger.Fatalf("cannot create backup: %s", err)
+	}
+	srcFS.MustStop()
+	dstFS.MustStop()
+	originFS.MustStop()
+}
+
+func usage() {
+	const s = `
+vmbackup performs backups for VictoriaMetrics data from instant snapshots to gcs, s3
+or local filesystem. Backed up data can be restored with vmrestore.
+
+See the docs at https://docs.victoriametrics.com/vmbackup.html .
+`
+	flagutil.Usage(s)
+}
+
+func newSrcFS() (*fslocal.FS, error) {
+	if len(*snapshotName) == 0 {
+		return nil, fmt.Errorf("`-snapshotName` or `-snapshot.createURL` must be provided")
+	}
+	snapshotPath := *storageDataPath + "/snapshots/" + *snapshotName
+
+	// Verify the snapshot exists.
+	f, err := os.Open(snapshotPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open snapshot at %q: %w", snapshotPath, err)
+	}
+	fi, err := f.Stat()
+	_ = f.Close()
+	if err != nil {
+		return nil, fmt.Errorf("cannot stat %q: %w", snapshotPath, err)
+	}
+	if !fi.IsDir() {
+		return nil, fmt.Errorf("snapshot %q must be a directory", snapshotPath)
+	}
+
+	fs := &fslocal.FS{
+		Dir:               snapshotPath,
+		MaxBytesPerSecond: maxBytesPerSecond.N,
+	}
+	if err := fs.Init(); err != nil {
+		return nil, fmt.Errorf("cannot initialize fs: %w", err)
+	}
+	return fs, nil
+}
+
+func newDstFS(dst string) (common.RemoteFS, error) {
+	fs, err := actions.NewRemoteFS(dst)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse `-dst`=%q: %w", dst, err)
+	}
+
+	return fs, nil
+}
+
+func newOriginFS() (common.OriginFS, error) {
+	if len(*origin) == 0 {
+		return &fsnil.FS{}, nil
+	}
+	fs, err := actions.NewRemoteFS(*origin)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse `-origin`=%q: %w", *origin, err)
+	}
+	return fs, nil
+}
